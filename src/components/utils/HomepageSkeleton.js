c573b79258622a58d4f312d7f05e1920f443a654
Shehroz Batt <101613341+ShehrozEdu@users.noreply.github.com>
2022-09-21 21:54:51 +0530

    Update HomepageSkeleton.js

--- src/components/utils/HomepageSkeleton.js
+++ src/components/utils/HomepageSkeleton.js
@@ -5,7 +5,19 @@ import "react-loading-skeleton/dist/skeleton.css";
 const HomepageSkeleton = () => {
   return (
     <>
-      <section className="px-0 d-flex box__section quick-search-item shadow mb-4 position-relative">
+      <section className="px-0 d-flex box__section quick-search-item shadow mb-4   me-3">
+        <Skeleton width={100} height={130} className="" />
+
+        <div className="pt-3 px-2 ">
+          <h4 className="indexColor fw-semibold">
+            <Skeleton width={100} />
+          </h4>
+          <p className="small text-muted">
+            <Skeleton width={200} />
+          </p>
+        </div>
+      </section>
+      <section className="px-0 d-flex box__section quick-search-item shadow mb-4   me-3">
         <Skeleton width={100} height={130} />
 
         <div className="pt-3 px-2 ">
@@ -16,6 +28,8 @@ const HomepageSkeleton = () => {
             <Skeleton width={200} />
           </p>
         </div>
+      </section>
+      <section className="px-0 d-flex box__section quick-search-item shadow mb-4   me-3">
         <Skeleton width={100} height={130} />
 
         <div className="pt-3 px-2 ">
@@ -26,17 +40,6 @@ const HomepageSkeleton = () => {
             <Skeleton width={200} />
           </p>
         </div>
-        <Skeleton width={100} height={130} />
-
-        <div className="pt-3 px-2 ">
-          <h4 className="indexColor fw-semibold">
-            <Skeleton width={100} />
-          </h4>
-          <p className="small text-muted">
-            <Skeleton width={200} />
-          </p>
-        </div>
-       
       </section>
     </>
   );
